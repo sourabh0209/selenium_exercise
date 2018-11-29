9bca4b848cd9876dffb5cd80faa71f8f4d03d94d
sourabh_gupta <gsourabh92@gmail.com>
2018-11-29 19:09:15 +0530

    Update admin_login.rb

--- exercise2/admin_login.rb
+++ exercise2/admin_login.rb
@@ -1,18 +1,28 @@
 require 'selenium-webdriver'
-Given(/^User must be an admin.$/)do
-  driver = Selenium::WebDriver.for :firefox
-  driver.navigate.to("http://web-dev.mypick.se/admin")
+EXPECTED_URL = "http://web-dev.mypick.se/sv/admin/admin_users"
+Given(/^User must be an admin.$/) do
+  @driver = Selenium::WebDriver.for :firefox
+  @driver.navigate.to("http://web-dev.mypick.se/admin")
 end
-When (/^I entered the Admin's Email as email$/)do
-  driver.find_element(id: 'user_email').send_keys('super_admin@mypick.com')
+
+When (/^I entered the Admin's Email as email$/) do
+  @driver.find_element(id: 'user_email').send_keys('super_admin@mypick.com')
 end
-And(/^I entered the password as password.$/)do
-  driver.find_element(id: 'user_password').send_keys('admin1234')    	
+
+And(/^I entered the password as password.$/) do
+  @driver.find_element(id: 'user_password').send_keys('admin1234')    	
 end
-Then(/^I should be redirected to the admin's home page.$/)do
-  driver.find_element(name: 'commit').click    	
+  
+Then(/^I should be redirected to the admin's home page.$/) do
+  @driver.find_element(name: 'commit').click 
 end
-And(/^Admin should be presented with the alert success message.$/)do
-  puts "Admin successfully logged in."
-  driver.quit    	
+
+And(/^Alert successfull message must be appeared.$/) do
+  existing_url = @driver.current_url
+  if (existing_url == EXPECTED_URL)
+    puts "Admin successfully logged in."
+  else
+  	puts "Admin failed to login"
+  end
+  @driver.quit
 end
